defc03823712032cf7592f1992bf39434421fc29
VonKruegs <rob.krueger@gmail.com>
2019-11-09 10:20:39 -0500

    Updated the Nav bar

--- booksearchapp/client/src/components/Nav/index.js
+++ booksearchapp/client/src/components/Nav/index.js
@@ -2,14 +2,21 @@ import React from "react";
 
 function Nav() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-danger">
-      <a className="navbar-brand" href="/">
-     Search Book
-      </a>
-      <a className="navbar-brand" href="/saved">
-      Saved Book List
-      </a>
-    </nav>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+  </button>
+  <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+    <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+    <li className="nav-item active">
+        <a className="nav-link" href="/">SEARCH AGAIN!</a>
+      </li>
+      <li className="nav-item active">
+        <a className="nav-link" href="/saved">SAVED BOOKS!</a>
+      </li>
+    </ul>
+  </div>
+</nav>
   );
 }
 
